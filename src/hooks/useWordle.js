bc85b8da256b429ab38e7b611bcb5546daa4e47a
Jurgen Hasmeta <jurgenhasmeta12@yahoo.com>
2022-01-27 13:51:03 +0100

    Final app version after lecture, just added new words + 100 of them

--- src/hooks/useWordle.js
+++ src/hooks/useWordle.js
@@ -1,6 +1,232 @@
 import { useCallback, useEffect, useState } from 'react'
 
-const words = ['potat', 'banan', 'apple']
+// const words = [
+//   'potat', 'banan', 'apple', 'noise', 'poops', 
+//   'stool', 'cooki', 'hamam', 'vacum', 
+//   'acnes', 'acoin', 'acorn', 'acres', 'actor', 'aeons', 'aires', 'aisle', 
+//   'alert', 'alien', 'alone', 'alter', 'anise', 'antes', 'antic', 
+//   'arise', 'arose', 'arson', 'aster', 'astir', 'atone',
+//   'cairn', 'calor', 'caner', 'canes', 'canoe', 'canto', 'cants', 'cares', 
+//   'carol', 'carts', 'caste', 'cater', 'cents', 'cites', 'clans', 'clean', 
+//   'clear', 'cleat', 'clone', 'close', 'clots', 'coals', 'coast', 'coats', 
+//   'coils', 'coins', 'colas', 'colts', 'cones', 'coral', 'corns', 'crane', 
+//   'crate', 'crest', 'cries', 'crone'
+// ]
+
+const words = [
+  'abuse',
+  'adult',
+  'agent',
+  'anger',
+  'apple',
+  'award',
+  'basis',
+  'beach',
+  'birth',
+  'block',
+  'blood',
+  'board',
+  'brain',
+  'bread',
+  'break',
+  'brown',
+  'buyer',
+  'cause',
+  'chain',
+  'chair',
+  'chest',
+  'chief',
+  'child',
+  'china',
+  'claim',
+  'class',
+  'clock',
+  'coach',
+  'coast',
+  'court',
+  'cover',
+  'cream',
+  'crime',
+  'cross',
+  'crowd',
+  'crown',
+  'cycle',
+  'dance',
+  'death',
+  'depth',
+  'doubt',
+  'draft',
+  'drama',
+  'dream',
+  'dress',
+  'drink',
+  'drive',
+  'earth',
+  'enemy',
+  'entry',
+  'error',
+  'event',
+  'faith',
+  'fault',
+  'field',
+  'fight',
+  'final',
+  'floor',
+  'focus',
+  'force',
+  'frame',
+  'frank',
+  'front',
+  'fruit',
+  'glass',
+  'grant',
+  'grass',
+  'green',
+  'group',
+  'guide',
+  'heart',
+  'henry',
+  'horse',
+  'hotel',
+  'house',
+  'image',
+  'index',
+  'input',
+  'issue',
+  'japan',
+  'jones',
+  'judge',
+  'knife',
+  'laura',
+  'layer',
+  'level',
+  'lewis',
+  'light',
+  'limit',
+  'lunch',
+  'major',
+  'march',
+  'match',
+  'metal',
+  'model',
+  'money',
+  'month',
+  'motor',
+  'mouth',
+  'music',
+  'night',
+  'noise',
+  'north',
+  'novel',
+  'nurse',
+  'offer',
+  'order',
+  'other',
+  'owner',
+  'panel',
+  'paper',
+  'party',
+  'peace',
+  'peter',
+  'phase',
+  'phone',
+  'piece',
+  'pilot',
+  'pitch',
+  'place',
+  'plane',
+  'plant',
+  'plate',
+  'point',
+  'pound',
+  'power',
+  'press',
+  'price',
+  'pride',
+  'prize',
+  'proof',
+  'queen',
+  'radio',
+  'range',
+  'ratio',
+  'reply',
+  'right',
+  'river',
+  'round',
+  'route',
+  'rugby',
+  'scale',
+  'scene',
+  'scope',
+  'score',
+  'sense',
+  'shape',
+  'share',
+  'sheep',
+  'sheet',
+  'shift',
+  'shirt',
+  'shock',
+  'sight',
+  'simon',
+  'skill',
+  'sleep',
+  'smile',
+  'smith',
+  'smoke',
+  'sound',
+  'south',
+  'space',
+  'speed',
+  'spite',
+  'sport',
+  'squad',
+  'staff',
+  'stage',
+  'start',
+  'state',
+  'steam',
+  'steel',
+  'stock',
+  'stone',
+  'store',
+  'study',
+  'stuff',
+  'style',
+  'sugar',
+  'table',
+  'taste',
+  'terry',
+  'theme',
+  'thing',
+  'title',
+  'total',
+  'touch',
+  'tower',
+  'track',
+  'trade',
+  'train',
+  'trend',
+  'trial',
+  'trust',
+  'truth',
+  'uncle',
+  'union',
+  'unity',
+  'value',
+  'video',
+  'visit',
+  'voice',
+  'waste',
+  'watch',
+  'water',
+  'while',
+  'white',
+  'whole',
+  'woman',
+  'world',
+  'youth'
+]
 
 function deepClone(data) {
   return JSON.parse(JSON.stringify(data))
@@ -96,24 +322,30 @@ export function useWordle() {
 
       // check only at-location
       for (const index in guess) {
+
         const char = guess[index]
         if (wordCopy[index] === char.value) {
           char.status = 'at-location'
           wordCopy = wordCopy.replace(char.value, '_')
         }
+
       }
 
       // check for in-word or not-in-word
       for (const index in guess) {
+
         const char = guess[index]
         if (char.status === 'at-location') continue
 
         if (wordCopy.includes(char.value)) {
           char.status = 'in-word'
           wordCopy = wordCopy.replace(char.value, '_')
-        } else {
+        } 
+        
+        else {
           char.status = 'not-in-word'
         }
+
       }
 
       setGuesses(guessesCopy)
@@ -121,8 +353,10 @@ export function useWordle() {
     },
     [guesses, word, position]
   )
+
   // enter a letter
   const enterCharacter = useCallback(
+
     function (char) {
       if (position.char >= 5) return
 
@@ -132,10 +366,12 @@ export function useWordle() {
       setPosition({ ...position, char: position.char + 1 })
     },
     [guesses, position]
+
   )
 
   // delete a character
   const deleteCharacter = useCallback(
+
     function () {
       if (position.char <= 0) return
 
@@ -145,6 +381,7 @@ export function useWordle() {
       setPosition({ ...position, char: position.char - 1 })
     },
     [guesses, position]
+
   )
 
   function reset() {
@@ -163,6 +400,7 @@ export function useWordle() {
 
   // listen to keyboard keys being typed
   useEffect(() => {
+
     if (finished) return
 
     const listener = e => {
@@ -176,6 +414,7 @@ export function useWordle() {
 
       // if user presses enter: submitGuess()
       if (key === 'enter') submitGuess()
+
     }
 
     window.addEventListener('keydown', listener)
@@ -183,4 +422,5 @@ export function useWordle() {
   }, [enterCharacter, deleteCharacter, submitGuess, finished])
 
   return { guesses, haveWon, reset, haveLost, word }
+
 }
